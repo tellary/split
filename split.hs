f20620fffdeac1ccc6645392ecee730adb843e61
Ilya Silvestrov <tellary@gmail.com>
2024-05-22 12:04:46 +0100

    First implementation of 'nullifyBalances'

---
+++ split.hs
@@ -0,0 +1,168 @@
+{-# LANGUAGE LambdaCase #-}
+
+import Data.Maybe (isJust, fromJust)
+import Data.List (sort, sortBy, sortOn, group, groupBy, find)
+import Data.List.Extra(groupOn)
+
+type Amount = Double
+type User = String
+data Account = UserAccount User deriving (Show, Eq, Ord)
+type DebitAccount = Account
+type CreditAccount = Account
+
+data Transaction
+  = Transaction
+  { txDebitAccount :: DebitAccount
+  , txCreditAccount :: CreditAccount
+  , txAmount :: Amount
+  } deriving (Show, Eq, Ord)
+
+mkTransaction :: Account -> Account -> Amount -> Transaction
+mkTransaction creditAcc debitAcc amount
+  | amount >= 0 = Transaction creditAcc debitAcc   amount
+  | otherwise   = Transaction debitAcc creditAcc (-amount)
+
+isCredit acc (Transaction _ creditAccount _) = acc == creditAccount
+isDebit acc (Transaction debitAccount _ _) = acc == debitAccount
+creditAmount acc tx@(Transaction _ _ amount)
+  | isCredit acc tx = Just amount
+  | otherwise = Nothing
+debitAmount acc tx@(Transaction _ _ amount)
+  | isDebit acc tx = Just amount
+  | otherwise = Nothing
+debitTransactions :: Account -> [Transaction] -> [Transaction]
+debitTransactions account = filter ((account ==) . txDebitAccount)
+creditTransactions :: Account -> [Transaction] -> [Transaction]
+creditTransactions account = filter ((account ==) . txCreditAccount)
+transactionAccountsDirectional :: Transaction -> (DebitAccount, CreditAccount)
+transactionAccountsDirectional (Transaction debitAccount creditAccount _)
+  = (debitAccount, creditAccount)
+transactionAccountsDirectionalArr :: Transaction -> [Account]
+transactionAccountsDirectionalArr (Transaction debitAccount creditAccount _)
+  = [debitAccount, creditAccount]
+transactionAccounts tx
+  = let [a1, a2] = sort . transactionAccountsDirectionalArr $ tx
+    in (a1, a2)
+transactionsAccounts
+  = map head . group . sort . concat .  map transactionAccountsDirectionalArr
+twoAccounts :: [Transaction] -> (Account, Account)
+twoAccounts txs
+  | length accs == 2 = (head accs, head . tail $ accs)
+  | otherwise = error $ "Only two accounts expected, but found " ++ show accs
+  where accs = transactionsAccounts txs
+  
+sameAccountsDirectional :: Transaction -> Transaction -> Bool
+sameAccountsDirectional tx1 tx2
+  = transactionAccountsDirectional tx1 == transactionAccountsDirectional tx2
+sameAccounts :: Transaction -> Transaction -> Bool
+sameAccounts tx1 tx2 = transactionAccounts tx1 == transactionAccounts tx2
+
+data Purchase = Purchase User Amount Split deriving (Show, Eq)
+data Split
+  = SplitEqually [User]
+  | SplitEquallyAll
+  deriving (Show, Eq)
+
+data Action = PurchaseAction Purchase | PaymentAction Transaction deriving Show
+data Actions = Actions [User] [Action] deriving Show
+
+actionsToTransactions :: Actions -> [Transaction]
+actionsToTransactions actions@(Actions _ actionsArr)
+  = concat . map (toTransactions actions) $ actionsArr
+
+toTransactions :: Actions -> Action -> [Transaction]
+toTransactions _ (PurchaseAction
+                  (Purchase debitUser amount (SplitEqually users)))
+  = concat
+    . map
+      (\case
+          user | user /= debitUser -> [ Transaction
+                                        (UserAccount debitUser)
+                                        (UserAccount user)
+                                        (amount/(fromIntegral $ length users))
+                                      ]
+               | otherwise -> []
+      )
+    $ users
+toTransactions
+  actions@(Actions users _)
+  (PurchaseAction
+    (Purchase debitUser amount SplitEquallyAll))
+  = toTransactions actions
+    (PurchaseAction
+     (Purchase debitUser amount (SplitEqually users)))
+
+balance :: Account -> [Transaction] -> Amount
+balance account transactions
+  = ( sum . fromJust . sequence . filter isJust . map (creditAmount account)
+      $ transactions )
+  - ( sum . fromJust . sequence . filter isJust . map (debitAmount account)
+      $ transactions )
+
+accountsBalances txs
+  = map (\acc -> (balance acc txs, acc)) accs
+  where
+    accs = transactionsAccounts txs
+
+collapseTwoAccountsTransactions txs
+  | balanceAmount >  0 = Just $ Transaction a1 a2 balanceAmount
+  | balanceAmount == 0 = Nothing
+  | otherwise          = Just $ Transaction a2 a1 (- balanceAmount)
+  where (a1, a2) = twoAccounts txs
+        balanceAmount = balance a2 txs
+
+collapseTransactions key
+  = map fromJust
+  . filter isJust
+  . map collapseTwoAccountsTransactions
+  . groupOn key
+  . sortOn key
+  . filter (\tx -> txDebitAccount tx /= txCreditAccount tx)
+
+collapseSameAccountsDirectional
+  = collapseTransactions transactionAccountsDirectional
+
+collapseSameAccounts
+  = collapseTransactions transactionAccounts
+
+decreaseBalance balances
+  = case (asc, desc) of
+      ((lowest, lowestAcc):_, (highest, highestAcc):_)
+        | abs lowest >= highest -> Transaction highestAcc lowestAcc  highest
+        | otherwise             -> Transaction highestAcc lowestAcc (abs lowest)
+  where
+    asc  = sort balances
+    desc = sortBy (flip compare) balances
+
+eqZero amt = abs amt < 0.001
+
+nullifyBalances0 newTxs txs
+  = case find (not . eqZero . fst) $ balances of
+      Just _  -> nullifyBalances0 (decreaseBalance balances:newTxs) txs
+      Nothing -> (newTxs, txs)
+  where
+    balances = accountsBalances allTxs
+    allTxs = newTxs ++ txs
+
+nullifyBalances = nullifyBalances0 []
+
+users = ["Serge", "Sasha", "Pasha", "Ilya", "Tasha", "Kolya", "Alena", "Dima"]
+
+actions
+  = Actions users
+    [ PurchaseAction (Purchase "Serge" 100.25 SplitEquallyAll)
+    , PurchaseAction (Purchase "Serge" 14.05 (SplitEqually ["Ilya"]))
+    , PurchaseAction (Purchase "Dima" 21.64 SplitEquallyAll)
+    , PurchaseAction (Purchase "Ilya" 14 SplitEquallyAll)
+    , PurchaseAction
+      (Purchase "Ilya" 24
+       (SplitEqually ["Ilya", "Kolya", "Alena", "Dima", "Tasha"]))
+    ]
+
+t1
+  = groupOn transactionAccounts
+  . sortOn transactionAccounts
+  . actionsToTransactions
+  $ actions
+
+t2 = nullifyBalances . actionsToTransactions $ actions
