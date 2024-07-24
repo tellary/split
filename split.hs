5683ea72da3d0d3e67322b8ba65db030b7d071ef
Ilya Silvestrov <tellary@gmail.com>
2024-07-24 18:10:42 +0100

    First implementation of 'addSplitAllPurchase' form

--- split.hs
+++ split.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE RecursiveDo #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
@@ -5,15 +6,20 @@
 {-# OPTIONS_GHC -Wno-missing-signatures -Wno-unused-top-binds #-}
 {-# OPTIONS_GHC -Wno-name-shadowing -Wno-unused-do-bind #-}
 
-import Data.List (delete)
-import Control.Monad.Fix
-import Control.Monad (forM)
-import Data.FileEmbed (embedFile)
-import Reflex.Dom --     (el, mainWidgetWithCss, text)
-import Data.Text (Text)
+import           Control.Monad     (forM)
+import           Control.Monad.Fix (MonadFix)
+import           Data.FileEmbed    (embedFile)
+import           Data.List         (delete)
+import           Data.Text         (Text)
+import qualified Data.Text         as T
+import           MoneySplit
+import           Reflex.Dom
+import           Text.Read         (readMaybe)
 
-addUserInput :: (DomBuilder t m, MonadHold t m, MonadFix m) => m (Event t Text)
-addUserInput = do
+resettableInput
+  :: (DomBuilder t m, MonadHold t m, MonadFix m)
+  => m (Event t Text)
+resettableInput = do
   rec
     input <- inputElement $ def & inputElementConfig_setValue .~ ("" <$ evText)
     let evEnter = keypress Enter input
@@ -35,7 +41,7 @@ manageUsers
   :: (Reflex t, DomBuilder t m, MonadHold t m, PostBuild t m, MonadFix m)
   => m (Dynamic t [Text])
 manageUsers = do
-  addUserEv <- addUserInput
+  addUserEv <- resettableInput
   rec
     users <-
       foldDyn ($) []
@@ -46,9 +52,48 @@ manageUsers = do
       )
     deleteUserEv <- switchHold never =<< dyn (displayUsers <$> users)
   return users
+
+addSplitAllPurchase
+  :: (DomBuilder t m, MonadHold t m, PostBuild t m, MonadFix m)
+  => m (Event t Purchase)
+addSplitAllPurchase = do
+  text "Add purchase"
+  el "br" blank
+  text "User: "
+  user <- value <$> inputElement def
+  el "br" blank
+  text "Description: "
+  desc <- value <$> inputElement def
+  el "br" blank
+  text "Amount: "
+  rec
+    amount <- fmap (readMaybe . T.unpack) <$> value <$> inputElement def
+    amountOnPress <- holdDyn (Just 0) (tagPromptlyDyn amount addEv)
+    text " "
+    dynText . ffor amountOnPress $ \case
+      Nothing -> "Bad amount"
+      Just _ -> ""
+    el "br" blank
+    addEv <- button "Add purchase"
+    let purchase
+          = do
+              userVal <- user
+              descVal <- desc
+              amountMaybe <- amount
+              return
+                $   Purchase
+                <$> pure (T.unpack userVal)
+                <*> pure (T.unpack descVal)
+                <*> amountMaybe
+                <*> pure SplitEquallyAll
+    let purchaseMaybeEv = tagPromptlyDyn purchase $ addEv
+  return . traceEvent "add purchase" . mapMaybe id $ purchaseMaybeEv
+
+manageActions = undefined
   
 main :: IO ()
 main = mainWidgetWithCss $(embedFile "split.css") $ do
   manageUsers
+  dynText =<< holdDyn "" =<< fmap (T.pack . show) <$> addSplitAllPurchase
   return ()
 
