699c9259c9e0621943d94285c58aa341666b644d
Ilya Silvestrov <tellary@gmail.com>
2024-08-26 23:10:42 +0100

    Upload only required files

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,13 @@ set -u
 nix-build -A ghcjs
 
 BUCKET=split.apps.tellary.ru
+FILES='index.html runmain.js all.min.js'
+
+aws s3api put-bucket-website --bucket $BUCKET --website-configuration file://website.json
 
 pushd result
 
-aws s3api put-bucket-website --bucket $BUCKET --website-configuration file://website.json
-ls -1 split.jsexe | xargs -I{} -n 1 -P 10 aws s3 cp split.jsexe/{} s3://$BUCKET/{} --metadata-directive REPLACE --cache-control max-age=60
-ls -1 split.jsexe | xargs -I{} -n 1 -P 10 aws s3api put-object-acl --bucket $BUCKET --key {} --acl public-read
+echo -n $FILES | xargs -d ' ' -I{} -n 1 -P 10 aws s3 cp split.jsexe/{} s3://$BUCKET/{} --metadata-directive REPLACE --cache-control max-age=60
+echo -n $FILES | xargs -d ' ' -I{} -n 1 -P 10 aws s3api put-object-acl --bucket $BUCKET --key {} --acl public-read
 
 popd
